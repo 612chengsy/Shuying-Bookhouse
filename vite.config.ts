435f0cc82cc78994358d2ccfc5249c1f8371a057
612chengsy <c13997674962@gmail.com>
2026-08-09 21:11:35 +0800

    V0.1.8增加后端

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Shuying-Bookhouse-V0.1.1/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
